0c3097457785f241cc8ce53ee42c9d0b6dfdcfc5
dennis-IT <dennis.huy.ta@gmail.com>
2020-12-07 18:36:10 -0500

    Added layout

---
+++ src/Layout1.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useState } from 'react';
+import { Transition, animated } from 'react-spring/renderprops';
+import Component1 from './components/Component1';
+import Component2 from './components/Component2';
+import Component3 from './components/Component3';
+
+const Layout1 = (props) => {
+    const [showComponent3, setShowComponent3] = useState(false);
+
+    const toggle = () => {
+        setShowComponent3(!showComponent3);
+    };
+
+    return (
+        <React.Fragment>
+            <h1>React-Spring Animation</h1>
+            <br />
+            <Component1 />
+            <Component2 toggle={toggle} />
+            <Transition
+                native
+                items={showComponent3}
+                from={{ opacity: 0 }}
+                enter={{ opacity: 1 }}
+                leave={{ opacity: 0 }}
+            >
+                {show => show && (props => (
+                    <animated.div style={props}>
+                        <Component3 />
+                    </animated.div>
+                ))}
+            </Transition>
+        </React.Fragment>
+    );
+};
+
+export default Layout1;
---
+++ src/Layout2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Layout2 = (props) => {
+    return (
+        <div>
+            <h1>AOS scrolling effect</h1>
+        </div>
+    );
+};
+
+export default Layout2;
